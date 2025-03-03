2415c04b6719d478ea927b39bbafae8b9664910e
Doanta Ginting <doantaaloycius@gmail.com>
2025-03-03 12:44:40 +0700

    Finished Moudle 4: Automata dan Table-Driven Construction

---
+++ 04_Automata_dan_Table-Driven_Construction/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+public class Program
+{
+    enum State {START, GAME, PAUSE, EXIT};
+    public static void  Main()
+    {
+        State state = State.START;
+        string[] screenName = {"START", "GAME", "PAUSE", "EXIT"};
+        while (state != State.EXIT)
+        {
+            Console.WriteLine(screenName[(int)state] + " SCREEN");
+            Console.Write("Enter Command : ");
+            string command = Console.ReadLine();
+            switch (state)
+            {
+                case State.START:
+                    if(command == "ENTER")
+                        state = State.GAME;
+                    else if (command == "QUIT")
+                        state = State.EXIT;
+                    else
+                        state = State.START;
+                    break;
+                case State.GAME:
+                    if(command == "ESC")
+                        state = State.PAUSE;
+                    else
+                        state = State.GAME;
+                    break;
+                case State.PAUSE:
+                    if(command == "BACK")
+                        state = State.GAME;
+                    else if (command == "HOME")
+                        state = State.START;
+                    else if (command == "QUIT")
+                        state = State.EXIT;
+                    else
+                        state = State.PAUSE;
+                    break;
+            }
+        }
+        Console.WriteLine("EXIT SCREEN");
+    }
+}
